8a674105629c32ae75ca2adeede02e38b7c8bdc8
hirata423 <ani335@icloud.com>
2022-02-22 19:41:11 +0900

    要調整2/22

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,40 @@ export const App = () => {
   const [centerIntervalKey, setCenterIntervalKey] = useState(0);
   const [rightIntervalKey, setRightIntervalKey] = useState(0);
 
+  const [allHandle, setAllHandle] = useState(true);
+
+  const [leftHandle, setLeftHandle] = useState(false);
+  const [centerHandle, setCenterHandle] = useState(false);
+  const [rightHandle, setRightHandle] = useState(false);
+
   //スロットスタートボタン
+
   const allSrotStartButton = () => {
-    leftSrotStart();
-    setleftIntervalKey(setInterval(leftSrotStart, 200));
-    centerSrotStart();
-    setCenterIntervalKey(setInterval(centerSrotStart, 175));
-    rightSrotStart();
-    setRightIntervalKey(setInterval(rightSrotStart, 150));
+    if (allHandle) {
+      leftSrotStart();
+      setleftIntervalKey(setInterval(leftSrotStart, 200));
+      document.getElementById("left").style.backgroundColor = "red";
+      centerSrotStart();
+      setCenterIntervalKey(setInterval(centerSrotStart, 175));
+      document.getElementById("center").style.backgroundColor = "red";
+      rightSrotStart();
+      setRightIntervalKey(setInterval(rightSrotStart, 150));
+      document.getElementById("right").style.backgroundColor = "red";
+
+      setLeftHandle(true);
+      setCenterHandle(true);
+      setRightHandle(true);
+
+      setAllHandle(false);
+      console.log("スタートされました");
+    } else if (leftHandle && centerHandle && rightHandle) {
+      setAllHandle(true);
+      // setLeftHandle(true);
+      // setCenterHandle(true);
+      // setRightHandle(true);
+
+      console.log("リセットします");
+    }
   };
 
   //左の処理【動】
@@ -38,6 +64,7 @@ export const App = () => {
   //左の処理【止】
   const leftSrotStopButton = () => {
     clearInterval(leftIntervalKey);
+    document.getElementById("left").style.backgroundColor = "blue";
   };
 
   //真ん中の処理【動】
@@ -48,6 +75,7 @@ export const App = () => {
   //真ん中の処理【止】
   const centerSrotStopButton = () => {
     clearInterval(centerIntervalKey);
+    document.getElementById("center").style.backgroundColor = "blue";
   };
 
   //右の処理【動】
@@ -58,6 +86,7 @@ export const App = () => {
   //右の処理【止】
   const rightSrotStopButton = () => {
     clearInterval(rightIntervalKey);
+    document.getElementById("right").style.backgroundColor = "blue";
   };
 
   return (
@@ -68,29 +97,45 @@ export const App = () => {
           <div className="leftSrot">
             <img src={leftSrot} />
             <br />
-            <button className="left" onClick={leftSrotStopButton}>
-              ストップ
-            </button>
+            {/* <button className="left" onClick={leftSrotStopButton}>ストップ</button> */}
+            <input
+              id="left"
+              type="button"
+              onClick={leftSrotStopButton}
+              value=""
+            ></input>
           </div>
           <div className="centerSrot">
             <img src={centerSrot} />
             <br />
-            <button className="center" onClick={centerSrotStopButton}>
-              ストップ
-            </button>
+            {/* <button className="center" onClick={centerSrotStopButton}>ストップ</button> */}
+            <input
+              id="center"
+              type="button"
+              onClick={centerSrotStopButton}
+              value=""
+            ></input>
           </div>
           <div className="rightSrot">
             <img src={rightSrot} />
             <br />
-            <button className="right" onClick={rightSrotStopButton}>
-              ストップ
-            </button>
+            {/* <button className="right" onClick={rightSrotStopButton}>ストップ</button> */}
+            <input
+              id="right"
+              type="button"
+              onClick={rightSrotStopButton}
+              value=""
+            ></input>
           </div>
         </div>
         <div className="mainBack">
-          <button className="risetButton" onClick={allSrotStartButton}>
-            スタート/ストップ
-          </button>
+          {/* <button className="risetButton" onClick={allSrotStartButton}>スタート/ストップ</button> */}
+          <input
+            id="all"
+            type="button"
+            className="risetButton"
+            onClick={allSrotStartButton}
+          ></input>
         </div>
       </div>
     </>
